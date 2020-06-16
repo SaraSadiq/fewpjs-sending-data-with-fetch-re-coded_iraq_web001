791870bda6817bb5d3cee909bdd7f009bf0317f0
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-16 21:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name,Email){
+return fetch('http://localhost:3000/users'{
+  method :"post",
+  header:{
+    'Content-Type':"application/json",
+    "Accept": "application/json"
+  }
+  body:json.stringify({
+    name,email
+  })
+})
+.then(function(response){
+  return response.json()
+} )
+.then (function(object){
+  document.body.innerHTML = object["id"]
+})
+.catch(function(error){
+  document.body.innerHTML = error.message
+})
+}
